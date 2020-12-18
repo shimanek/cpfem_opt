c4cccce3a1443f26183cc4477d1fc0101eff795f
John Shimanek <john@Johns-MacBook-Pro.local>
2020-12-18 13:34:31 -0500

    new input comments for max_strain

--- opt_fea.py
+++ opt_fea.py
@@ -24,18 +24,18 @@ else:
 ### user input
 param_list = ['Tau0', 'H0', 'TauS', 'hs', 'gamma0']
 param_bounds = [ (1,100), (100,500), (1,200), (0,100), (0.0001,0.4) ]
-loop_len = 300
-n_initial_points = 100
-large_error = 5e3  # backup RMSE of runs which don't finish; first option uses 1.5 * IQR(first few RMSE)
+loop_len = 150
+n_initial_points = 50
+large_error = 5e3  
+# ^ backup RMSE of runs which don't finish; first option uses 1.5 * IQR(first few RMSE)
 exp_SS_file = [f for f in os.listdir(os.getcwd()) if f.startswith('exp')][0]
 length = 9
 area = 9 * 9
 jobname = 'UT_729grains'
 recursion_depth = 3
-max_strain = 0.05 #as a percentage
+max_strain = 0.05
+# ^ set to 0 to go to max value in experimental data file
 ### end input
-
-
 
 def main():
     remove_out_files()
