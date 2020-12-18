db518e083067a49092083b705d72d20ed3329fd8
John Shimanek <john@Johns-MacBook-Pro.local>
2020-12-18 13:35:17 -0500

    set_strain_inp able to use max exp value

--- opt_fea.py
+++ opt_fea.py
@@ -84,6 +84,10 @@ def set_strain_inp():
     global max_strain
     global length
     global exp_SS_file
+
+    if max_strain == 0:
+        max_strain = max(np.loadtxt( exp_SS_file, skiprows=1, delimiter=',' )[:,0])
+
     max_bound = round(max_strain * length, 4) #round to 4 digits
 
     filename = [ f for f in os.listdir(os.getcwd()) if f.startswith('UT') and f.endswith('.inp')][0]
