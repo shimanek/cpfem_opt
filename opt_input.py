92a258c8c5b24887bf776709dd05e2a4b0e30ec0
John Shimanek <john@Johns-MacBook-Pro.local>
2021-01-16 13:02:22 -0500

    large attempt to access user settings from its global object

--- opt_input.py
+++ opt_input.py
@@ -8,10 +8,10 @@ loop_len = 150
 n_initial_points = 50
 large_error = 5e3  
 # ^ backup RMSE of runs which don't finish; first option uses 1.5 * IQR(first few RMSE)
-# exp_SS_file = [f for f in os.listdir(os.getcwd()) if f.startswith('exp')][0]
+exp_SS_file = [f for f in os.listdir(os.getcwd()) if f.startswith('exp')][0]
 length = 9
 area = 9 * 9
 jobname = 'UT_729grains'
-recursion_depth = 3
+recursion_depth = 2
 max_strain = 0.0
 # ^ 0 for max exp value, fractional strain (0.01=1%) otherwise
