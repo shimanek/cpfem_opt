46a49024dec07400d2364c2bc691a5537f726994
John Shimanek <john@Johns-MacBook-Pro.local>
2021-01-15 22:41:36 -0500

    first pass at importing settings as py file

--- opt_fea.py
+++ opt_fea.py
@@ -21,21 +21,24 @@ else:
     from odbMaterial import *
     from odbSection import *
 
-### user input
-param_list = ['Tau0', 'H0', 'TauS', 'hs', 'gamma0']
-param_bounds = [ (1,100), (100,500), (1,200), (0,100), (0.0001,0.4) ]
-loop_len = 150
-n_initial_points = 50
-large_error = 5e3  
-# ^ backup RMSE of runs which don't finish; first option uses 1.5 * IQR(first few RMSE)
+if os.path.isfile('opt_input.py'):
+    from opt_input import *
+else:
+    ### user input if no input file
+    param_list = ['Tau0', 'H0', 'TauS', 'hs', 'gamma0']
+    param_bounds = [ (1,100), (100,500), (1,200), (0,100), (0.0001,0.4) ]
+    loop_len = 150
+    n_initial_points = 50
+    large_error = 5e3  
+    # ^ backup RMSE of runs which don't finish; first option uses 1.5 * IQR(first few RMSE)
+    length = 9
+    area = 9 * 9
+    jobname = 'UT_729grains'
+    recursion_depth = 3
+    max_strain = 0.0
+    # ^ 0 for max exp value, fractional strain (0.01=1%) otherwise
+    ### end input
 exp_SS_file = [f for f in os.listdir(os.getcwd()) if f.startswith('exp')][0]
-length = 9
-area = 9 * 9
-jobname = 'UT_729grains'
-recursion_depth = 3
-max_strain = 0.0
-# ^ 0 for max exp value, fractional strain (0.01=1%) otherwise
-### end input
 
 def main():
     remove_out_files()
