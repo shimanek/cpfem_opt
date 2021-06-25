ec04eb348d452a74f3e6dc5dd3f81c54d23c3bc2
John Shimanek <jshimanek@psu.edu>
2021-06-25 12:43:32 -0400

    input options: change to simplified case for testing

--- opt_input.py
+++ opt_input.py
@@ -2,14 +2,14 @@
 User settings for opt_fea.py and opt_plot.py
 """
 import os
-param_list = ['Tau0', 'H0', 'TauS', 'hs', 'gamma0']
-param_bounds = [ (1,100), (100,500), (1,200), (0,100), (0.0001,0.4) ]
+param_list = ['Tau0', 'H0']
+param_bounds = [(1,100), (100,500)]
 loop_len = 150
 n_initial_points = 50
 large_error = 5e3  
 # ^ backup RMSE of runs which don't finish; first option uses 1.5 * IQR(first few RMSE)
 exp_SS_file = [f for f in os.listdir(os.getcwd()) if f.startswith('exp')][0]
-param_file = [f for f in os.listdir(os.getcwd()) if f.startswith('Mat_BW')][0]
+param_file = [f for f in os.listdir(os.getcwd()) if f.startswith('mat_param')][0]
 length = 9
 area = 9 * 9
 jobname = 'UT_729grains'
