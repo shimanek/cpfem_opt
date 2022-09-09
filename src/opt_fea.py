8ef11b2777add2b69b0187f46ed1eb44b53fb8d0
John Shimanek <jshimanek@psu.edu>
2022-09-09 11:15:17 -0400

    FIX newline for each error line output

--- src/opt_fea.py
+++ src/opt_fea.py
@@ -98,10 +98,10 @@ def write_error_to_file(error_list, orient_list):
     error_fname = 'out_errors.txt'
     if os.path.isfile(error_fname):
         with open(error_fname, 'a+') as f:
-            f.write(','.join([str(err) for err in error_list + [np.mean(error_list)]]))
+            f.write('\n' + ','.join([str(err) for err in error_list + [np.mean(error_list)]]))
     else:
         with open(error_fname, 'w+') as f:
-            f.write('# errors for {} and mean error\n'.format(orient_list))
+            f.write('# errors for {} and mean error'.format(orient_list))
 
 
 class ExpData():
