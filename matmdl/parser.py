1e30a7a1a3fbcdb72b82e558913fa894523c89fb
shimanek <jshimanek@psu.edu>
2023-11-16 09:02:08 -0500

    ADD: default option handling

--- matmdl/parser.py
+++ matmdl/parser.py
@@ -28,6 +28,8 @@ class UserSettings:
 				self.upper = kwargs.get("upper")
 			else:
 				self.upper = None
+			if 'default' in kwargs:
+				self.default = kwargs.get("default")
 
 	input_reqs = {
 		'run': {
@@ -45,7 +47,7 @@ class UserSettings:
 			'cpus': Option(types=[int]),
 			'do_load_previous': Option(types=[bool, int]),
 			'is_compression': Option(types=[bool]),
-			'slope_weight': Option(types=[int,float], crit=False),
+			'slope_weight': Option(types=[int,float], crit=False)#, default=0.4),
 		},
 		'plot': {
 			'grain_size_name': Option(crit=False, types=[str]),
@@ -54,7 +56,9 @@ class UserSettings:
 		},
 	}
 
+
 	def __init__(self):
+		categories = ["run", "plot"]
 		with open(input_fname, "rb") as f:
 			conf = tomllib.load(f)
 
@@ -67,11 +71,22 @@ class UserSettings:
 					self.orientations[orient['name']] = orient
 
 			# get all input:
-			for category in ['run', 'plot']:
+			for category in categories:
 				for key, value in conf[category].items():
 					if key not in self.input_reqs[category].keys():
 						raise AttributeError(f'Unknown input: {key}')
 					self.__dict__[key] = value
+
+			# check if defaults needed:
+			for category in categories:
+				for key, value in self.input_reqs[category].items():
+					if key not in self.__dict__:
+						try:
+							print(f"Input warning: input {key} not found, using default value of {value.default}")
+							self.__dict__[key] = value.default
+						except AttributeError:
+							raise AttributeError(f"\nInput: no defualt found for option {key}\n")
+
 
 		# general checks:
 		for key, req in self.input_reqs['run'].items():
