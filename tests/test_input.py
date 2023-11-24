2136cf67dd284e45d4ab60d551403b616b239d41
shimanek <jshimanek@psu.edu>
2023-11-24 17:01:51 -0500

    ADD: option for per orientation and global max/min strain limits

--- tests/test_input.py
+++ tests/test_input.py
@@ -1,6 +1,22 @@
 import unittest
 from matmdl.parser import uset
 import numpy as np
+import os
+
+
+class TestExp(unittest.TestCase):
+	def _by_orientation_name(self, exp, orient_name):
+		data_out = np.loadtxt(f"exp_{orient_name}.csv", delimiter=",")
+		parsed_data = exp.data[orient_name]['raw']
+		equal_elements = np.equal(data_out,parsed_data)
+		self.assertTrue(equal_elements.all())
+
+	def test_data_limits(self):
+		from matmdl.experimental import ExpData
+		exp = ExpData(uset.orientations)
+		self._by_orientation_name(exp, "test")
+		self._by_orientation_name(exp, "001")
+		os.remove("temp_expSS.csv")
 
 
 class TestError(unittest.TestCase):
